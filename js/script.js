5548e7b2af2a2b7a0263f5d3985a7af5b16157cc
Валерия Морева <valeriamoreva@Valerias-MacBook-Air.local>
2023-11-03 17:46:23 +0300

    Изменения функционала в js файле

--- js/script.js
+++ js/script.js
@@ -5,18 +5,17 @@ let breakTittle = document.getElementById('break');
 
 let colorHead = document.getElementById('head');
 
-let workTime = 25;
+let workTime = 1;
 let breakTime = 5;
 
 let switchTime = workTime;
 
-let secondsStandart = "00";
-let seconds = 0;
+let seconds = "00";
 
 // display
 window.onload = () => {
     document.getElementById('minutes').innerHTML = workTime;
-    document.getElementById('seconds').innerHTML = secondsStandart;
+    document.getElementById('seconds').innerHTML = seconds;
 
     document.getElementById('head').style.background = '#987645'
     document.getElementById('ear-left').style.background = '#654321'
@@ -27,6 +26,23 @@ window.onload = () => {
     workTittle.classList.add('active');
 }
 
+function getNull(num) {
+
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+const changeGradientColor = (procent = 100) => {
+
+    document.getElementById('head').style.background = `linear-gradient(to top, #987645 ${procent}%, white 0%)`;
+    document.getElementById('ear-left').style.background = `linear-gradient(to right, #654321 ${procent}%, white 0%)`;
+    document.getElementById('ear-right').style.background = `linear-gradient(to left, #654321 ${procent}%, white 0%)`;
+    document.getElementById('face').style.background = `linear-gradient(to bottom, #654321 ${procent}%, white 0%)`;
+
+}
 // start timer
 function start() {
     // change button
@@ -35,59 +51,28 @@ function start() {
 
     // change the time
     seconds = 59;
-    secondsStandart = 59
 
     let workMinutes = workTime - 1;
     let breakMinutes = breakTime - 1;
 
     breakCount = 1;
+    let procent, time, minuteInsecond = 0;
 
     // countdown
     let timerFunction = () => {
         //change the display
         document.getElementById('minutes').innerHTML = workMinutes;
-        document.getElementById('seconds').innerHTML = secondsStandart;
+        document.getElementById('seconds').innerHTML = seconds;
 
         // start
-        seconds = seconds - 1;
-
-        //changeColorPug
-
-        if(breakCount % 2 === 0) {
-            secondsStandart = seconds.toString().length === 1 ? "0" + seconds : seconds;
-
-            let time = (workMinutes * 60) + secondsStandart
-    
-            let minuteInsecond = (switchTime * 60);
-            
-            let procent = 100 - ((time * 100)/minuteInsecond);
-            procent = 100
-            document.getElementById('head').style.background = `linear-gradient(to top, #987645 ${procent}%, white 0%)`
-            document.getElementById('ear-left').style.background = `linear-gradient(to right, #654321 ${procent}%, white 0%)`
-            document.getElementById('ear-right').style.background = `linear-gradient(to left, #654321 ${procent}%, white 0%)`
-            document.getElementById('face').style.background = `linear-gradient(to bottom, #654321 ${procent}%, white 0%)`
-        } else {
-            secondsStandart = seconds.toString().length === 1 ? "0" + seconds : seconds;
-
-            let time = (workMinutes * 60) + secondsStandart
-    
-            let minuteInsecond = (switchTime * 60);
-            
-            let procent = 100 - ((time * 100)/minuteInsecond);
-        
-            document.getElementById('head').style.background = `linear-gradient(to top, #987645 ${procent}%, white 0%)`
-            document.getElementById('ear-left').style.background = `linear-gradient(to right, #654321 ${procent}%, white 0%)`
-            document.getElementById('ear-right').style.background = `linear-gradient(to left, #654321 ${procent}%, white 0%)`
-            document.getElementById('face').style.background = `linear-gradient(to bottom, #654321 ${procent}%, white 0%)`
-    
-        }
+        seconds = getNull(seconds - 1);
 
         //changeTime
-        if(seconds === 0) {
+        if (seconds === '00') {
             workMinutes = workMinutes - 1;
-    
-            if(workMinutes === -1 ){
-                if(breakCount % 2 !== 0) {
+
+            if (workMinutes === -1) {
+                if (breakCount % 2 !== 0) {
                     // start break
                     workMinutes = breakMinutes;
                     switchTime = 0;
@@ -96,7 +81,7 @@ function start() {
 
                     workTittle.classList.remove('active');
                     breakTittle.classList.add('active');
-                }else {
+                } else {
                     // continue work
                     workMinutes = workTime - 1;
                     switchTime = workTime;
@@ -108,8 +93,24 @@ function start() {
             }
             seconds = 59;
         }
+
+        //changeColorPug
+        if (breakCount % 2 == 0) {
+           
+            changeGradientColor()
+
+        } else {
+
+            time = (workMinutes * 60) + seconds
+
+            minuteInsecond = (switchTime * 60);
+
+            procent = 100 - ((time * 100) / minuteInsecond);
+            
+            changeGradientColor(procent)
+
+        }
     }
-    
 
     // start countdown
     setInterval(timerFunction, 1000); // 1000 = 1s
@@ -117,7 +118,6 @@ function start() {
 
 let startTimer = document.getElementById('start');
 
-startTimer.onclick = function() 
-{
+startTimer.onclick = function () {
     start();
 }
